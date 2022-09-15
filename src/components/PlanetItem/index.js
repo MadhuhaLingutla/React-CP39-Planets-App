5697ff32a32ce57b3e902ed7d2d823f503f54cc0
Madhuha Lingutla <madhuha5695@gmail.com>
2022-09-15 16:57:50 +0000

    Solution revision

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -4,9 +4,10 @@ import './index.css'
 const PlanetItem = props => {
   const {planetDetails} = props
   const {name, imageUrl, description} = planetDetails
+  const planetImageAlt = `planet ${name}`
   return (
     <div className="planet-container">
-      <img src={imageUrl} alt={name} className="planet-image" />
+      <img src={imageUrl} alt={planetImageAlt} className="planet-image" />
       <h1 className="planet-name">{name}</h1>
       <p className="planet-description">{description}</p>
     </div>
--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -2,9 +2,6 @@
 
 import Slider from 'react-slick'
 
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-
 import PlanetItem from '../PlanetItem'
 
 import './index.css'
